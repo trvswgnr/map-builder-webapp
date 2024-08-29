6f2f7fe92e8c18dd1d08e8e01dc9611a08c9d875
Travis Wagner <travis.wagner@umusic.com>
2024-08-28 23:09:56 -0400

    remove all border radius

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,9 +53,15 @@ module.exports = {
         },
       },
       borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
+        none: '0',
+        sm: '0',
+        DEFAULT: '0',
+        md: '0',
+        lg: '0',
+        xl: '0',
+        '2xl': '0',
+        '3xl': '0',
+        full: '0',
       },
       keyframes: {
         "accordion-down": {
