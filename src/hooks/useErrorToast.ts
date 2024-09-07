027a13c07331ead823b3d16a0a18be34120ea6a7
Travis Wagner <travis.wagner@umusic.com>
2024-09-06 21:00:44 -0400

    rename

--- src/hooks/useErrorToast.ts
+++ src/hooks/useErrorToast.ts
@@ -1,5 +1,5 @@
 // hooks/useErrorToast.ts
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/hooks/useToast";
 
 export function useErrorToast() {
   const { toast } = useToast();
